ba818f75693d2bf092fc6a03c8fe841cfe267245
Amitrajit Ghosh <amitrajitg5@gmail.com>
2022-05-22 01:57:15 +0800

    backend

--- src/components/Register.js
+++ src/components/Register.js
@@ -51,10 +51,16 @@ function Register() {
                 pass: pass
               }).then((e) => {
                 var uid = ""
-                uid = e['data']['uid']          
+                uid = e['data']['uid']
+
+
+            
+              
+                  const socket = io.connect("https://bifrost-messenger.herokuapp.com/")
+                
                 
                 if(uid.length > 0){
-                  navigate("../home", {state: {email:email}});
+                  navigate("../home", {state: {email:email, socket:socket}});
                 }
             }).catch((e) => {
               console.log(e)
